e01bcf906cdf56b7edb729fa48d80c2f77e901a8
edouard steiner <boursoracle@free.fr>
2026-02-21 22:42:31 +0100

    add audio folder

--- app_edouard.py
+++ app_edouard.py
@@ -1,8 +1,6 @@
-import pandas as pd
-import plotly.express as px
+import streamlit as st
 import os
 from datetime import datetime
-import streamlit as st
 
 st.markdown("# Web App d'Edouard")
 
@@ -11,14 +9,25 @@ audio_value = st.audio_input("Record a voice message")
 if audio_value:
     st.audio(audio_value)
 
-    data_dir = os.path.join(os.getcwd(), "audio")
-
+    # dossier pour enregistrer temporairement le fichier
+    data_dir = "audio"
     os.makedirs(data_dir, exist_ok=True)
 
+    # créer un nom unique avec timestamp
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     audio_path = os.path.join(data_dir, f"audio_{timestamp}.wav")
 
+    # sauvegarde du fichier audio
     with open(audio_path, "wb") as f:
         f.write(audio_value.getbuffer())
 
     st.success(f"Audio saved to {audio_path}")
+
+    # bouton pour télécharger l'audio sur ton ordinateur
+    with open(audio_path, "rb") as f:
+        st.download_button(
+            label="Download audio",
+            data=f,
+            file_name=f"audio_{timestamp}.wav",
+            mime="audio/wav"
+        )
